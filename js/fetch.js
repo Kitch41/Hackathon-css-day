6bdad9b0d90dd66be4ae3467d09cf86284c2753f
Kitch41 <stefkeuken@hotmail.com>
2024-03-26 10:14:01 +0100

    fetch data

---
+++ js/fetch.js
@@ -0,0 +1,21 @@
+async function fetchData() {
+    try {
+        const response = await fetch("https://cssday.nl/data.json");
+
+        if (!response.ok) {
+            throw new Error("Failed to get data");
+        }
+
+        const data = await response.json();
+
+        console.log(data)
+        return data;
+        
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error; 
+    }
+}
+
+fetchData();
